024b93e5f63fb6c9c2fec29bab683ec3c91f7572
Ted Wong <WongMingYin@gmail.com>
2018-05-09 16:46:47 +1000

    Update startTrim.sh

--- STAGE1/startTrim.sh
+++ STAGE1/startTrim.sh
@@ -7,7 +7,6 @@
 directory=$1
 my_samples=$(cat $directory/samples.txt)
 
-cd /project/RDS-SMS-WGS22-RW
 for sname in $my_samples; do
     A=$(qsub -v sampleID=$sname STAGE1/1_HPC_hg38_TRIMMOMATIC.pbs) 
 done;
