b5a9a2c0deea4b1bb8d9e888b5d6e59b0c206495
Ted Wong <WongMingYin@gmail.com>
2018-05-09 12:12:22 +1000

    Update PREPARE_CANONICAL_FASTA.sh

--- STAGE1/PREPARE_CANONICAL_FASTA.sh
+++ STAGE1/PREPARE_CANONICAL_FASTA.sh
@@ -1,25 +1,23 @@
-hg38='/home/CMRI/eteber/das/Process/Bioinformatics/eteber/hg38bundle/hg38bundle/broad_38_canonical.fasta'
-REF='/home/CMRI/eteber/das/Process/Bioinformatics/eteber/hg38bundle/hg38bundle/broad_hg38_canonical_with_vectors.fasta'
-DS='/home/CMRI/eteber/das/Process/Bioinformatics/eteber/hg38bundle/hg38bundle'
+#!/bin/bash
 
-SV40='pRSVT_seq.fasta'
-HPVE6E7='HPV_E6E7_seq.fasta'
-EBV='NC_007605_EBV.fasta'
-pLKO1='pLKO1_scrambled_seq.fasta'
-pLXSN='pLXSN_seq.fasta'
-pBABE='pBABE_puro_seq.fasta'
+#PBS -l walltime=10:00:00
+#PBS -P RDS-FMH-CWGS-RW
+#PBS -l select=1:ncpus=1
+#PBS -q defaultQ
 
-cd $DS
+module load samtools
+
+# Reference FASTA
+FASTA='/home/CMRI/eteber/das/Process/Bioinformatics/eteber/hg38bundle/hg38bundle/broad_38_canonical.fasta'
+
+# Where for BWA
+BWA='/tools/bwa/0.7.10'
+
+# Where for SAMTools
+SAMTOOLS='samtools'
 
 #/tools/samtools/1.3/samtools faidx broad_38.fasta
 #/tools/samtools/1.3/samtools faidx broad_38.fasta chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM  > broad_38_canonical.fasta
 
-time cat $hg38 $EBV $SV40 $pLKO1 $pLXSN $HPVE6E7 $pBABE > $REF
- 
-SM='/tools/samtools/1.3'
-MP='/tools/bwa/0.7.10'
-
-time $SM/samtools faidx $REF 
-time $MP/bwa index -a bwtsw $REF
-
-cat $REF | grep '>'
+time $SAMTOOLS faidx $FASTA 
+time $BWA index -a bwtsw $FASTA
