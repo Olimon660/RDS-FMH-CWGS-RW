9cbe5efd8f0d001c1041c472e5b3cfd8fc3b132f
Ted Wong <WongMingYin@gmail.com>
2018-05-14 12:42:10 +1000

    Update PREPARE_CANONICAL_FASTA.sh

--- STAGE1/PREPARE_CANONICAL_FASTA.sh
+++ STAGE1/PREPARE_CANONICAL_FASTA.sh
@@ -19,5 +19,5 @@ SAMTOOLS='samtools'
 #/tools/samtools/1.3/samtools faidx broad_38.fasta
 #/tools/samtools/1.3/samtools faidx broad_38.fasta chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM  > broad_38_canonical.fasta
 
-time $SAMTOOLS faidx $FASTA 
-time $BWA index -a bwtsw $FASTA
+$SAMTOOLS faidx $FASTA 
+$BWA index -a bwtsw $FASTA
