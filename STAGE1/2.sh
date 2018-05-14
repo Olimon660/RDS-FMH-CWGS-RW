9620ac7e87d8e820ca74d3e05d9ee22fabc61b41
Ted Wong <WongMingYin@gmail.com>
2018-05-15 03:21:06 +1000

    Update 2.sh

--- STAGE1/2.sh
+++ STAGE1/2.sh
@@ -7,6 +7,6 @@
 samples=$(cat $2)
 
 for name in $samples; do
-    echo 'qsub -v sampleID=$name -v in=$1 STAGE1/1_HPC_hg38_TRIMMOMATIC.pbs'
-    qsub -v sm=$name -v in=$1 STAGE1/1_HPC_hg38_TRIMMOMATIC.pbs
+    echo $name
+    #qsub -v sm=$name -v in=$1 STAGE1/1_HPC_hg38_TRIMMOMATIC.pbs
 done;
