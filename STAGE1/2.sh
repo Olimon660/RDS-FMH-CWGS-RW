a6ef2919cf105b379853699db886d79e2115cc21
Ted Wong <WongMingYin@gmail.com>
2018-05-15 03:53:26 +1000

    Update 2.sh

--- STAGE1/2.sh
+++ STAGE1/2.sh
@@ -1,5 +1,5 @@
 #
-# ./STAGE1/2.sh <INPUT> <SAMPLE_TXT>
+# ./STAGE1/2.sh <SAMPLE_TXT>
 #
 
 #!/bin/bash 
@@ -7,6 +7,6 @@
 samples=$(cat $2)
 
 for name in $samples; do
-    A=$(qsub -v in=$1 -v sm=$name STAGE1/1_HPC_hg38_TRIMMOMATIC.pbs) 
+    A=$(qsub -v sm=$name STAGE1/1_HPC_hg38_TRIMMOMATIC.pbs) 
     echo $name $A
 done;
