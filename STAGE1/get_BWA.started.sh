b9e1bf5af788aafb2b1ce666ede1735cea2d92d6
Ted Wong <WongMingYin@gmail.com>
2018-05-17 17:13:04 +1000

    Create get_BWA.started.sh

---
+++ STAGE1/get_BWA.started.sh
@@ -0,0 +1,12 @@
+#
+# ./STAGE1/get_BWA.started.sh
+#
+
+#!/bin/bash 
+
+samples=$(cat samples.txt)
+
+for name in $samples; do
+    A=$(qsub -v sm=$name STAGE1/4_HPC_hg38_alignment.pbs.pbs) 
+    echo $name $A
+done;
