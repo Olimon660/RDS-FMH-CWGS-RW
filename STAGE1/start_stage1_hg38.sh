211455af1e1cb35ce1946402d4f14865136a86cf
Ted Wong <WongMingYin@gmail.com>
2018-05-22 15:13:47 +1000

    Update start_stage1_hg38.sh

--- STAGE1/start_stage1_hg38.sh
+++ STAGE1/start_stage1_hg38.sh
@@ -7,34 +7,34 @@ cd /project/RDS-FMH-CWGS-RW
 
 for name in $samples; do
     # MARK DUPS
-    G=$(qsub -v sampleID=$name 7_HPC_hg38_markdups.pbs) 
+    G=$(qsub -v sampleID=$name STAGE1/7_HPC_hg38_markdups.pbs) 
     echo $sname $G 
 	
 	# DEL SORTED BAM
-	#H=$(qsub -W depend=afterok:$G -v sampleID=$name 8_HPC_hg38_del_sorted_bam.pbs)
+	#H=$(qsub -W depend=afterok:$G -v sampleID=$name STAGE1/8_HPC_hg38_del_sorted_bam.pbs)
        # echo $sname $H
 
 	# READGROUPS
-	#I=$(qsub -W depend=afterok:$H -v sampleID=$name 9_HPC_hg38_readgroups.pbs) 
+	#I=$(qsub -W depend=afterok:$H -v sampleID=$name STAGE1/9_HPC_hg38_readgroups.pbs) 
 	#echo $sname $I
 
 	# DEL_DEDUP
-	#J=$(qsub -W depend=afterok:$I -v sampleID=$name 10_HPC_hg38_delete_dedup.pbs)
+	#J=$(qsub -W depend=afterok:$I -v sampleID=$name STAGE1/10_HPC_hg38_delete_dedup.pbs)
        # echo $sname $J
 
 	# INDEX
-	#K=$(qsub -W depend=afterok:$J -v sampleID=$name 11_HPC_hg38_index.pbs) 
+	#K=$(qsub -W depend=afterok:$J -v sampleID=$name 1STAGE1/1_HPC_hg38_index.pbs) 
 	#echo $sname $K
  
 	# RealignerTargetCreator
-	#L=$(qsub -W depend=afterok:$K -v sampleID=$name 12_HPC_hg38_RealignerTarget.pbs) 
+	#L=$(qsub -W depend=afterok:$K -v sampleID=$name STAGE1/12_HPC_hg38_RealignerTarget.pbs) 
 	#echo $sname $L
 
 	# IndelRealigner
-	#M=$(qsub -W depend=afterok:$L -v sampleID=$name 13_HPC_hg38_IndelRealign.pbs) 
+	#M=$(qsub -W depend=afterok:$L -v sampleID=$name STAGE1/13_HPC_hg38_IndelRealign.pbs) 
 	#echo $sname $M
 
         # FINAL INDEX
-       # N=$(qsub -W depend=afterok:$M -v sampleID=$name 14_HPC_hg38_final_index.pbs)
+       # N=$(qsub -W depend=afterok:$M -v sampleID=$name STAGE1/14_HPC_hg38_final_index.pbs)
        # echo $sname $N
 done;
