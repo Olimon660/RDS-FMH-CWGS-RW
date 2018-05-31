3ed7b78546a63000a4d28e7d0ad8cfe33bf9b617
Ted Wong <WongMingYin@gmail.com>
2018-05-31 15:19:44 +1000

    Update start_stage2_HAPLOTYPECALLER.sh

--- STAGE2/start_stage2_HAPLOTYPECALLER.sh
+++ STAGE2/start_stage2_HAPLOTYPECALLER.sh
@@ -4,6 +4,6 @@ file=$1
 samples=$(cat $file)
 
 for name in $samples; do
-    GATK=$(qsub -v sampleID=$name HAPLOTYPECALLER.pbs)
+    #GATK=$(qsub -v sampleID=$name HAPLOTYPECALLER.pbs)
     echo $t_name $GATK
 done;
