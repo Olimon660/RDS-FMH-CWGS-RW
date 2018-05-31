39a53137903913d5bef9e31ee3664b9f4a3cbe89
Ted Wong <WongMingYin@gmail.com>
2018-05-31 15:19:15 +1000

    Update start_stage2_HAPLOTYPECALLER.sh

--- STAGE2/start_stage2_HAPLOTYPECALLER.sh
+++ STAGE2/start_stage2_HAPLOTYPECALLER.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 
-my_file='/project/RDS-SMS-WGS22-RW/haplotype_input_file_names.txt'
-END=$(cat $my_file | wc -l)
-START=1
+file=$1
+samples=$(cat $file)
 
-for i in $(eval echo "{$START..$END}");
- do
-   	t_name=$(head -"$i" $my_file | tail -1 | cut -f1)
-   	echo $t_name
+for name in $samples; do
+    t_name=$(head -"$i" $my_file | tail -1 | cut -f1)
+    echo $t_name
 	
-   	GATK=$(qsub -v sampleID=$t_name HAPLOTYPECALLER.pbs)
-        echo $t_name $GATK
+    GATK=$(qsub -v sampleID=$t_name HAPLOTYPECALLER.pbs)
+    echo $t_name $GATK
 done;
