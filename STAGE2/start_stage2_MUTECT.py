2b109c11f028332d5f49fd0dd5148daedd71ae54
Ted Wong <WongMingYin@gmail.com>
2018-06-12 16:30:08 +1000

    Update start_stage2_MUTECT.py

--- STAGE2/start_stage2_MUTECT.py
+++ STAGE2/start_stage2_MUTECT.py
@@ -5,8 +5,8 @@ with open(sys.argv[1]) as r:
     for line in r:
         toks = line.strip().split('\t') 
         assert(len(toks) == 2)
-        normal = toks[0]
-        tumor  = toks[1]               
+        tumor  = toks[0]
+        normal = toks[1]
         #cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' STAGE2/MUTECT_with_control.pbs'
         cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' STAGE2/TEST.pbs'        
         os.system(cmd)
