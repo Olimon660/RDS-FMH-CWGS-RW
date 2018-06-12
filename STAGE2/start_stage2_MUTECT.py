ed7127338cf3c1b1ad422597863de317c40ed104
Ted Wong <WongMingYin@gmail.com>
2018-06-12 14:10:10 +1000

    Update start_stage2_MUTECT.py

--- STAGE2/start_stage2_MUTECT.py
+++ STAGE2/start_stage2_MUTECT.py
@@ -7,6 +7,6 @@ with open(sys.argv[1]) as r:
         assert(len(toks) == 2)
         normal = toks[0]
         tumor  = toks[1]               
-        #cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' MUTECT_with_control.pbs'
-        cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' TEST.pbs'        
+        #cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' STAGE2/MUTECT_with_control.pbs'
+        cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' STAGE2/TEST.pbs'        
         os.system(cmd)
