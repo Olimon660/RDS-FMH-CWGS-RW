9f23433b9b4949040650dca1438836a8b2465c67
Ted Wong <WongMingYin@gmail.com>
2018-06-12 12:21:43 +1000

    Update start_stage2_MUTECT.sh

--- STAGE2/start_stage2_MUTECT.sh
+++ STAGE2/start_stage2_MUTECT.sh
@@ -10,12 +10,6 @@ for i in $(eval echo "{$START..$END}");
 	n_name=$(head -"$i" $my_file | tail -1 | cut -f2)
 	echo $t_name $n_name
 
-	if [ $n_name != "NONE" ]
-	then
-   	    MUTECT=$(qsub -v sampleID=$t_name,controlID=$n_name MUTECT_with_control.pbs)
-            echo $t_name $n_name $MUTECT
-	else
-   	    MUTECT=$(qsub -v sampleID=$t_name MUTECT_without_control.pbs)
-            echo $t_name $MUTECT
-	fi
+        MUTECT=$(qsub -v sampleID=$t_name,controlID=$n_name MUTECT_with_control.pbs)
+        echo $t_name $n_name $MUTECT
 done;
