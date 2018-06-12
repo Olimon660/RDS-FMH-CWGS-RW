ad1afe377c87567140d26b1f35e091cc41a18a87
Ted Wong <WongMingYin@gmail.com>
2018-06-12 16:44:59 +1000

    Update start_stage2_MUTECT.py

--- STAGE2/start_stage2_MUTECT.py
+++ STAGE2/start_stage2_MUTECT.py
@@ -5,6 +5,12 @@ with open(sys.argv[1]) as r:
     for line in r:
         toks = line.strip().split('\t') 
         assert(len(toks) == 2)
+        
+        #
+        # The file follows Erdahl's Excel. IMMORTAL is on first column and is tumor, while MORTAL is normal on the
+        # second column.
+        #
+        
         tumor  = toks[0]
         normal = toks[1]
         #cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' STAGE2/MUTECT_with_control.pbs'
