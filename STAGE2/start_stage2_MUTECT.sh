cda06eb3ce46e4f798a0aa711248ad5e8c908487
Ted Wong <WongMingYin@gmail.com>
2018-06-12 12:22:45 +1000

    Update start_stage2_MUTECT.sh

--- STAGE2/start_stage2_MUTECT.sh
+++ STAGE2/start_stage2_MUTECT.sh
@@ -6,10 +6,10 @@ START=1
 
 for i in $(eval echo "{$START..$END}");
     do
-        t_name=$(head -"$i" $my_file | tail -1 | cut -f1)
-	n_name=$(head -"$i" $my_file | tail -1 | cut -f2)
-	echo $t_name $n_name
+        normal=$(head -"$i" $my_file | tail -1 | cut -f1)
+	tumor=$(head -"$i" $my_file | tail -1 | cut -f2)
+	echo $normal $tumor
 
-        MUTECT=$(qsub -v sampleID=$t_name,controlID=$n_name MUTECT_with_control.pbs)
+        MUTECT=$(qsub -v normalID=$normal,tumorID=$tumor MUTECT_with_control.pbs)
         echo $t_name $n_name $MUTECT
 done;
