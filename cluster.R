f3c985ed920c08d588962e80c6111e510b92bdab
Ted Wong <WongMingYin@gmail.com>
2018-06-14 12:37:56 +1000

    Update cluster.R

--- cluster.R
+++ cluster.R
@@ -1,22 +1,34 @@
 library(VariantAnnotation)
 
 path <- '/Users/twong/Sources/RDS-FMH-CWGS-RW/6'
+#path <- '/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/6'
 
-parse <- function(x)
+setwd(path)
+files <- list.files(path, pattern = "\\vcf$")
+data <- data.frame()
+
+for (file in files)
 {
-    vcf <- readVcf(x, "hg19")    
+    vcf <- readVcf(file, "hg19")
     CHR <- as.character(seqnames(vcf))
     POS <- start(ranges(vcf))
     REF <- as.character(ref(vcf))
     ALT <- as.character(unlist(alt(vcf)))
-    key <- paste(CHR,POS,REF,ALT,sep='_')
-    key
+    key <- paste(CHR, POS, REF, ALT, sep='_')
+    data  <- rbind(data, data.frame(Key=key, Sample=file))
+    print(file)
 }
 
-setwd(path)
-files <- list.files(path, pattern = "\\vcf$")
-data <- lapply(files, parse)
+uniq <- unique(sort(data$Key))
+print(paste(c('There are'), length(uniq), 'unique variants'))
 
-d <- proxy::dist(data, method = "Jaccard")
+m <- matrix(data=0, nrow=length(files), ncol=length(uniq))
+rownames(m) <- files
+colnames(m) <- uniq
+
+ix <- unique(cbind(as.character(data$Sample), as.character(data$Key)))
+m[ix] <- 1
+
+d <- proxy::dist(m, method='Jaccard')
 h = hclust(d)
 plot(h)
