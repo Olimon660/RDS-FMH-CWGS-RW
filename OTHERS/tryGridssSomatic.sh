0135a0ffd9a1fcaf15b887b11e03151e3f251fb3
student-t <WongMingYin@gmail.com>
2018-06-18 12:59:43 +1000

    Update tryGridssSomatic.sh

--- OTHERS/tryGridssSomatic.sh
+++ OTHERS/tryGridssSomatic.sh
@@ -3,11 +3,12 @@
 NORMAL=REALIGNED_RG_DEDUP_SORTED_HG19_JFCF6_P12_H06L4ALXX_6.bam
 TUMOUR=REALIGNED_RG_DEDUP_SORTED_HG19_JFCF6_T_1J_1-3C_H06L4ALXX_3.bam # The one Erdhal asked for
 
-java -jar gridss-1.7.2-gridss-jar-with-dependencies.jar \
+java -ea -Xmx31g \
 	-Dsamjdk.use_async_io_read_samtools=true \
 	-Dsamjdk.use_async_io_write_samtools=true \
 	-Dsamjdk.use_async_io_write_tribble=true \
 	-Dsamjdk.compression_level=1 \
+	-cp gridss-1.7.2-gridss-jar-with-dependencies.jar gridss.CallVariants \
 	WORKER_THREADS=20 \
 	ASSEMBLY=gridss_JFCF6_SV.bam \
 	INPUT="$NORMAL" \
