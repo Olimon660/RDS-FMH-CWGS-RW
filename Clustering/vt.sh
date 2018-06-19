a3fac2566de5b35184deb640bfbfa3ee80acf72f
student-t <WongMingYin@gmail.com>
2018-06-20 00:49:17 +1000

    Update vt.sh

--- Clustering/vt.sh
+++ Clustering/vt.sh
@@ -2,7 +2,7 @@
 
 OUT='/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/6'
 
-for file in $OUT/*.vcf; do
+for file in $OUT/SNP_*.vcf; do
     echo vt decompose -s $file -o $OUT/DECOMPOSED_$(basename "$file")
     vt decompose -s $file -o $OUT/DECOMPOSED_$(basename "$file")
 done
