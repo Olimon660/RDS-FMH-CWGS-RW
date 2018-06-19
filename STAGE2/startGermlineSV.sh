891653dfc76eb2547d99d42a150f7cb9144ad369
student-t <WongMingYin@gmail.com>
2018-06-19 15:02:58 +1000

    Update startGermlineSV.sh

--- STAGE2/startGermlineSV.sh
+++ STAGE2/startGermlineSV.sh
@@ -4,6 +4,6 @@ file=$1
 samples=$(cat $file)
 
 for name in $samples; do
-    GATK=$(qsub -v sampleID=$name STAGE2/HAPLOTYPECALLER.pbs)
-    echo $name $GATK
+    x=$(qsub -v sampleID=$name STAGE2/germlineSV.pbs)
+    echo $name $x
 done;
