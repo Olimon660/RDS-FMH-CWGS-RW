8bd22c122e7b7e538d605c5fc77160aa058ca583
student-t <WongMingYin@gmail.com>
2018-06-26 15:44:02 +1000

    Update SOMATIC_ANNOTATION.sh

--- Somatic/SOMATIC_ANNOTATION.sh
+++ Somatic/SOMATIC_ANNOTATION.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-IX='/scratch/RDS-FMH-CWGS-RW/Sources/6'
-OX='/scratch/RDS-FMH-CWGS-RW/Sources/6'
+IX='/scratch/RDS-FMH-CWGS-RW/Sources/7'
+OX='/scratch/RDS-FMH-CWGS-RW/Sources/7'
 
 for file in $IX/REMOVED_FILTERED_*.vcf; do
     qsub -v file=$file Somatic/SOMATIC_ANNOTATION.pbs
