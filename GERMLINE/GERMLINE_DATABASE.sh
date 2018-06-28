991c4467075890c4bd6c04f3aec677360f2d9ebd
student-t <WongMingYin@gmail.com>
2018-06-28 16:47:58 +1000

    Update GERMLINE_DATABASE.sh

--- GERMLINE/GERMLINE_DATABASE.sh
+++ GERMLINE/GERMLINE_DATABASE.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
-for file in 6/ANNOTATED_REMOVED_FILTERED_*vcf; do
+#for file in 6/ANNOTATED_REMOVED_FILTERED_*vcf; do
+for file in 6/REMOVED_FILTERED_INDEL_NORM_DECOM_GATK_ZK-58*.vcf; do
     qsub -v file=$file OTHERS/DATABASE.pbs
 done;
