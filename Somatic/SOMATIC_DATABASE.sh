416e833b4cf4d82cdb347c3390467f775263c57e
student-t <WongMingYin@gmail.com>
2018-06-28 18:07:06 +1000

    Create SOMATIC_DATABASE.sh

---
+++ Somatic/SOMATIC_DATABASE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#PBS -l walltime=72:00:00
+#PBS -P RDS-FMH-CWGS-RW
+#PBS -l select=1:ncpus=1:mem=32GB
+#PBS -q defaultQ
+
+module load python
+python OTHERS/ANNOTATION_SQL.py OTHERS/ANNOTATION.sql 6 6/6.csv
