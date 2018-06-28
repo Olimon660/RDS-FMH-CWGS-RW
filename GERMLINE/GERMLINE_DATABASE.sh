d176e10babe42d9d558dfff0278b7b8718eb2944
student-t <WongMingYin@gmail.com>
2018-06-28 16:26:40 +1000

    Update GERMLINE_DATABASE.sh

--- GERMLINE/GERMLINE_DATABASE.sh
+++ GERMLINE/GERMLINE_DATABASE.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
 for file in 6/ANNOTATED_REMOVED_FILTERED_*vcf; do
-    echo $file
-#    $(qsub -v file=$file OTHERS/DATABASE.pbs)
+    $(qsub -v file=$file OTHERS/DATABASE.pbs)
 done;
