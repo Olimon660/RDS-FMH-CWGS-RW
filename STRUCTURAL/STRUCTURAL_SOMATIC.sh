1e8224d97abb87b2a73f5abba047e17a058090e3
student-t <WongMingYin@gmail.com>
2018-06-29 20:46:13 +1000

    Update STRUCTURAL_SOMATIC.sh

--- STRUCTURAL/STRUCTURAL_SOMATIC.sh
+++ STRUCTURAL/STRUCTURAL_SOMATIC.sh
@@ -16,19 +16,19 @@ REFERENCE="$REF"
 OUTPUT=${OX}/SOMATIC_GRIDSS_$TUMO.vcf
 ASSEMBLY=${OX}/SOMATIC_GRIDSS_$TUMO.bam
 
-#echo 'java -ea -Xmx128g \
-#	-Dsamjdk.create_index=true \
-#	-Dsamjdk.use_async_io_read_samtools=true \
-#	-Dsamjdk.use_async_io_write_samtools=true \
-#	-Dsamjdk.use_async_io_write_tribble=true \
-#	-Dsamjdk.compression_level=1 \
-#	-cp /scratch/RDS-FMH-CWGS-RW/softwares/gridss-1.7.2-gridss-jar-with-dependencies.jar gridss.CallVariants \
-#	TMP_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
-#	WORKER_THREADS=2 \
-#	WORKING_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
-#	REFERENCE_SEQUENCE="$REFERENCE" \
-#	INPUT="$NORMAL" \
-#	INPUT="$TUMOR" \
-#	OUTPUT="$OUTPUT" \
-#	ASSEMBLY="$ASSEMBLY" \
-#	2>&1 | tee -a gridss.somatic."${TUMO}".$$.log'
+echo 'java -ea -Xmx128g \
+	-Dsamjdk.create_index=true \
+	-Dsamjdk.use_async_io_read_samtools=true \
+	-Dsamjdk.use_async_io_write_samtools=true \
+	-Dsamjdk.use_async_io_write_tribble=true \
+	-Dsamjdk.compression_level=1 \
+	-cp /scratch/RDS-FMH-CWGS-RW/softwares/gridss-1.7.2-gridss-jar-with-dependencies.jar gridss.CallVariants \
+	TMP_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
+	WORKER_THREADS=2 \
+	WORKING_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
+	REFERENCE_SEQUENCE="$REFERENCE" \
+	INPUT=$NORMAL \
+	INPUT=$TUMOR \
+	OUTPUT=$OUTPUT \
+	ASSEMBLY=$ASSEMBLY \
+	2>&1 | tee -a gridss.somatic.$TUMO.$$.log'
