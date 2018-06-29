f578ec2e2c1188d9e4fe1a0b1d68083474f4c1bc
student-t <WongMingYin@gmail.com>
2018-06-29 20:42:44 +1000

    Update STRUCTURAL_SOMATIC.sh

--- STRUCTURAL/STRUCTURAL_SOMATIC.sh
+++ STRUCTURAL/STRUCTURAL_SOMATIC.sh
@@ -6,29 +6,31 @@ REF='/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/combi
 
 NORM="$1"
 TUMO="$2"
+echo $NORM
+echo $TUMO
 
-NORMAL=${IS}/REALIGNED_RG_DEDUP_SORTED_HG19_"$(NORM)".bam
-TUMOR=${DS}/REALIGNED_RG_DEDUP_SORTED_HG19_"${TUMO}".bam
+#NORMAL=${IS}/REALIGNED_RG_DEDUP_SORTED_HG19_"$(NORM)".bam
+#TUMOR=${DS}/REALIGNED_RG_DEDUP_SORTED_HG19_"${TUMO}".bam
 
-REFERENCE="$(REF)"
-OUTPUT=${OX}/SOMATIC_GRIDSS_"${TUMO}".vcf
-ASSEMBLY=${OX}/SOMATIC_GRIDSS_"${TUMO}".bam
+#REFERENCE="$(REF)"
+#OUTPUT=${OX}/SOMATIC_GRIDSS_"${TUMO}".vcf
+#ASSEMBLY=${OX}/SOMATIC_GRIDSS_"${TUMO}".bam
 
-cd /scratch/RDS-FMH-CWGS-RW/Sources/8
+#cd /scratch/RDS-FMH-CWGS-RW/Sources/8
 
-echo 'java -ea -Xmx128g \
-	-Dsamjdk.create_index=true \
-	-Dsamjdk.use_async_io_read_samtools=true \
-	-Dsamjdk.use_async_io_write_samtools=true \
-	-Dsamjdk.use_async_io_write_tribble=true \
-	-Dsamjdk.compression_level=1 \
-	-cp /scratch/RDS-FMH-CWGS-RW/softwares/gridss-1.7.2-gridss-jar-with-dependencies.jar gridss.CallVariants \
-	TMP_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
-	WORKER_THREADS=2 \
-	WORKING_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
-	REFERENCE_SEQUENCE="$REFERENCE" \
-	INPUT="$NORMAL" \
-	INPUT="$TUMOR" \
-	OUTPUT="$OUTPUT" \
-	ASSEMBLY="$ASSEMBLY" \
-	2>&1 | tee -a gridss.somatic."${TUMO}".$$.log'
+#echo 'java -ea -Xmx128g \
+#	-Dsamjdk.create_index=true \
+#	-Dsamjdk.use_async_io_read_samtools=true \
+#	-Dsamjdk.use_async_io_write_samtools=true \
+#	-Dsamjdk.use_async_io_write_tribble=true \
+#	-Dsamjdk.compression_level=1 \
+#	-cp /scratch/RDS-FMH-CWGS-RW/softwares/gridss-1.7.2-gridss-jar-with-dependencies.jar gridss.CallVariants \
+#	TMP_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
+#	WORKER_THREADS=2 \
+#	WORKING_DIR=/scratch/RDS-FMH-CWGS-RW/Sources/8 \
+#	REFERENCE_SEQUENCE="$REFERENCE" \
+#	INPUT="$NORMAL" \
+#	INPUT="$TUMOR" \
+#	OUTPUT="$OUTPUT" \
+#	ASSEMBLY="$ASSEMBLY" \
+#	2>&1 | tee -a gridss.somatic."${TUMO}".$$.log'
