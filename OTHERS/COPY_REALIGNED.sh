31f5c90f37afeb90af9eec96c47fff459b6a8bf5
student-t <WongMingYin@gmail.com>
2018-07-03 15:12:39 +1000

    Update COPY_REALIGNED.sh

--- OTHERS/COPY_REALIGNED.sh
+++ OTHERS/COPY_REALIGNED.sh
@@ -4,5 +4,5 @@ OUT='/scratch/RDS-FMH-CWGS-RW/Sources/5'
 
 for file in $OUT/REALIGNED_RG_DEDUP_SORTED_HG19_*.bam; do    
     #qsub -v file=$file OTHERS/COPY_REALIGNED.pbs
-    echo 'qsub -v file=$"file" OTHERS/COPY_REALIGNED.pbs'
+    echo 'qsub -v file="$file" OTHERS/COPY_REALIGNED.pbs'
 done
