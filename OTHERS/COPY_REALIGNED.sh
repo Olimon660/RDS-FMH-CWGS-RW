462de84a7386caf05e40fb2a80eeeed24bd9689d
student-t <WongMingYin@gmail.com>
2018-07-03 14:53:33 +1000

    Update COPY_REALIGNED.sh

--- OTHERS/COPY_REALIGNED.sh
+++ OTHERS/COPY_REALIGNED.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 
-#PBS -l walltime=96:00:00
-#PBS -P RDS-FMH-CWGS-RW
-#PBS -l select=1:ncpus=1:mem=16GB
-#PBS -q defaultQ
+OUT='/scratch/RDS-FMH-CWGS-RW/Sources/5'
 
-cd /scratch/RDS-FMH-CWGS-RW/Sources
-scp 5/REALIGNED_RG_DEDUP_SORTED_HG19_*bam ubuntu@43.241.202.39:/home/ubuntu/keep1/8/
-
+for file in $OUT/REALIGNED_RG_DEDUP_SORTED_HG19_*.bam; do    
+    qsub -v file=$file OTHERS/COPY_REALIGNED.pbs
+done
