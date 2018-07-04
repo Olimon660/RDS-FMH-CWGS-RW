d9c124a4ee74b2765153c6e1b4452b2a64b5da6c
student-t <WongMingYin@gmail.com>
2018-07-04 22:11:41 +1000

    Update STRUCTURAL_USYD.py

--- STRUCTURAL/STRUCTURAL_USYD.py
+++ STRUCTURAL/STRUCTURAL_USYD.py
@@ -1,5 +1,5 @@
 #
-# python STRUCTURAL/STRUCTURAL_USYD.py samples.txt
+# python STRUCTURAL/STRUCTURAL_USYD.py STAGE2/case_control_input_files.txt
 #
 
 import os
@@ -7,8 +7,17 @@ import sys
 
 with open(sys.argv[1]) as r:
     for line in r:
-        sample = line.strip()
+        toks = line.strip().split('\t') 
+        assert(len(toks) == 2)
         
-        cmd = 'qsub -v sample=' + sample + ' STRUCTURAL/STRUCTURAL_SOMATIC.pbs'
+        #
+        # The file follows Erdahl's Excel. IMMORTAL is on first column and is tumor, while MORTAL is normal on the
+        # second column.
+        #
+        
+        tumor  = toks[0]
+        normal = toks[1]
+        
+        cmd = 'qsub -v normalID=' + normal + ',tumorID=' + tumor + ' STRUCTURAL/STRUCTURAL_SOMATIC.pbs'
         print(cmd)
         os.system(cmd)
