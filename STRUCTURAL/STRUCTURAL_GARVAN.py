724a87c2ac7ee7c35b2df02932bfb4043d166c52
student-t <WongMingYin@gmail.com>
2018-07-05 17:53:32 +1000

    Update STRUCTURAL_GARVAN.py

--- STRUCTURAL/STRUCTURAL_GARVAN.py
+++ STRUCTURAL/STRUCTURAL_GARVAN.py
@@ -13,6 +13,10 @@ with open(sys.argv[1]) as r:
         tumor  = toks[0]
         normal = toks[1]
         
-        cmd = 'qsub -m beas -pe smp 2 -l mem_requested=16G,tmp_requested=16G -q short.q -V -cwd  -j y -S /bin/sh STRUCTURAL/STRUCTURAL_GARVAN.pbs ' + normal + ' ' + tumor
+        cmd = 'STRUCTURAL/STRUCTURAL_GARVAN.pbs ' + normal + ' ' + tumor
+        cmd = 'qsub -q short.q -m beas -pe smp 2 -l mem_requested=16G,tmp_requested=16G -V -cwd -j y -S ' + cmd
+        
+        qsub -q short.q -pe smp 8 -l mem_requested=16G,tmp_requested=60G COMMAND
+        
         print(cmd)
         os.system(cmd)
