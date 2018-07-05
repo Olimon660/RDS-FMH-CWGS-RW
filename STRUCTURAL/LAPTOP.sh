05bbbe0e6e0a45b56441291accf5c24474f5269c
student-t <WongMingYin@gmail.com>
2018-07-05 15:31:47 +1000

    Update LAPTOP.sh

--- STRUCTURAL/LAPTOP.sh
+++ STRUCTURAL/LAPTOP.sh
@@ -1,4 +1,3 @@
-
 scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_LFS05F24post.bam .
 scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_LFS05F24pre.bam .
 scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_MeT_4Apost.bam .
