ffffbae831065fe93f53bcb13eccda490d218a21
Ted Wong <twong@cmri-bioinfo4.cmri.com.au>
2018-07-05 13:48:40 +1000

    ...

--- Clustering/vt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-OUT='/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/6'
-
-for file in $OUT/SNP_*.vcf; do
-    #echo vt decompose -s $file -o $OUT/DECOMPOSED_$(basename "$file")
-    vt decompose -s $file -o $OUT/DECOMPOSED_$(basename "$file")
-done
