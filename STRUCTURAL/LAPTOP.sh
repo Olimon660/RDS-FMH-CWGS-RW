396f1784978ab6f4e17c791467da1ced898ffee4
student-t <WongMingYin@gmail.com>
2018-07-05 15:31:37 +1000

    Update LAPTOP.sh

--- STRUCTURAL/LAPTOP.sh
+++ STRUCTURAL/LAPTOP.sh
@@ -1,7 +1,7 @@
 
-scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_LFS05F24post.bam . > /dev/null
-scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_LFS05F24pre.bam . > /dev/null
-scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_MeT_4Apost.bam . > /dev/null
-scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_MeT_4Apre.bam . > /dev/null
-scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_VA13.bam . > /dev/null
-scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_WI38.bam . > /dev/null
+scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_LFS05F24post.bam .
+scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_LFS05F24pre.bam .
+scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_MeT_4Apost.bam .
+scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_MeT_4Apre.bam .
+scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_VA13.bam .
+scp twon2259@hpc.sydney.edu.au:/scratch/RDS-FMH-CWGS-RW/Sources/5/REALIGNED_RG_DEDUP_SORTED_HG19_WI38.bam .
