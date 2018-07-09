4884b4115aae48b9115107a34ae27c8f25d9156f
student-t <WongMingYin@gmail.com>
2018-07-09 19:26:27 +1000

    Update STRUCTURAL_ANNOTATION.sh

--- STRUCTURAL/STRUCTURAL_ANNOTATION.sh
+++ STRUCTURAL/STRUCTURAL_ANNOTATION.sh
@@ -3,6 +3,6 @@
 IX='/scratch/RDS-FMH-CWGS-RW/Sources/8'
 OX='/scratch/RDS-FMH-CWGS-RW/Sources/8'
 
-for file in $IX/REMOVED_FILTERED_*.vcf; do
+for file in $IX/SOMATIC_GRIDSS_*.vcf; do
     qsub -v file=$file GERMLINE/GERMLINE_ANNOTATION.pbs
 done
