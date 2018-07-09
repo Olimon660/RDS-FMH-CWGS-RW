940b79b265e65de77801d60a20345f00e430f752
student-t <WongMingYin@gmail.com>
2018-07-09 19:25:13 +1000

    Update STRUCTURAL_ANNOTATION.sh

--- STRUCTURAL/STRUCTURAL_ANNOTATION.sh
+++ STRUCTURAL/STRUCTURAL_ANNOTATION.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-IX='/scratch/RDS-FMH-CWGS-RW/Sources/6'
-OX='/scratch/RDS-FMH-CWGS-RW/Sources/6'
+IX='/scratch/RDS-FMH-CWGS-RW/Sources/8'
+OX='/scratch/RDS-FMH-CWGS-RW/Sources/8'
 
 for file in $IX/REMOVED_FILTERED_*.vcf; do
     qsub -v file=$file GERMLINE/GERMLINE_ANNOTATION.pbs
