49ff9325efb60a7a9540f873298935a4fbd9ae97
student-t <WongMingYin@gmail.com>
2018-07-11 14:21:24 +1000

    Update STATS_ALIGNMENT.sh

--- STATS/STATS_ALIGNMENT.sh
+++ STATS/STATS_ALIGNMENT.sh
@@ -3,6 +3,6 @@
 IX='/scratch/RDS-FMH-CWGS-RW/Sources/5'
 
 #for file in $IX/REALIGNED*.bam; do
-for file in $IX/REALIGNED_RG_DEDUP_SORTED_HG19_*ZK-58.bam; do
+for file in $IX/*A.bam*; do
     qsub -v file=$file STATS/STATS_ALIGNMENT.pbs
 done
