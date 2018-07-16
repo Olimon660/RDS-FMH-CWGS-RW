ef1b43539a1c9828af15dbaabdbee5e886e037ae
student-t <WongMingYin@gmail.com>
2018-07-16 14:08:32 +1000

    Update STRUCTURAL_SOMATIC.sh

--- STRUCTURAL/STRUCTURAL_SOMATIC.sh
+++ STRUCTURAL/STRUCTURAL_SOMATIC.sh
@@ -3,6 +3,8 @@
 IX='/scratch/RDS-FMH-CWGS-RW/Sources/8'
 OX='/scratch/RDS-FMH-CWGS-RW/Sources/8'
 
+module load R
+
 for file in $IX/SOMATIC_GRIDSS_*.vcf; do
-    qsub -v file=$file STRUCTURAL/STRUCTURAL_ANNOTATION.pbs
+    qsub -v file=$file STRUCTURAL/STRUCTURAL_SOMATIC.pbs
 done
