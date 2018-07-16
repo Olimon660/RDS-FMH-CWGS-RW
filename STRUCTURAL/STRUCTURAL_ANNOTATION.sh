029a351fe1b96d3f0744bd39a3bd95dfc021dd46
student-t <WongMingYin@gmail.com>
2018-07-16 19:19:15 +1000

    Update STRUCTURAL_ANNOTATION.sh

--- STRUCTURAL/STRUCTURAL_ANNOTATION.sh
+++ STRUCTURAL/STRUCTURAL_ANNOTATION.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IX='/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/8'
-OX='/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/8'
+IX='8'
+OX='8'
 
 for file in $IX/SOMATIC_GRIDSS_*.vcf; do
     java -Xmx16g -jar /home/twong/snpEff/snpEff.jar hg19 $IX/$(basename ${file}) > $OX/ANNOTATED_$(basename ${file})
