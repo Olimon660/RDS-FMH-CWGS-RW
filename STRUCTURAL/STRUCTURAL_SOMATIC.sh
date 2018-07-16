0e3d081d768946dd498691bc630379c7de3243cf
student-t <WongMingYin@gmail.com>
2018-07-16 20:01:54 +1000

    Update STRUCTURAL_SOMATIC.sh

--- STRUCTURAL/STRUCTURAL_SOMATIC.sh
+++ STRUCTURAL/STRUCTURAL_SOMATIC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-IX='/home/twong/das_manual/Process/Bioinformatics/tedwong/RDS-FMH-CWGS-RW/8'
+IX='8'
 
 for file in $IX/SOMATIC_GRIDSS_*.vcf; do
     Rscript STRUCTURAL/STRUCTURAL_SOMATIC.R $file $file.bed
