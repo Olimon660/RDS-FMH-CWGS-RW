0f9f09800b5ea7cf3e45545d61fa9ce583ec0e67
Ted Wong <ted@pacificanalytics.com.au>
2018-09-18 17:59:07 +1000

    ...

--- AN1/AN1_EXTRACT.py
+++ AN1/AN1_EXTRACT.py
@@ -6,6 +6,7 @@
 
 import sys
 
+# https://wiki.cmri.com.au/pages/viewpage.action?pageId=22578249
 keys = [ "ENSG00000204209", "ENSG00000085224", "ENSG00000164362", "ENSG00000141510" ]
 
 with open(sys.argv[1], "r") as r:
