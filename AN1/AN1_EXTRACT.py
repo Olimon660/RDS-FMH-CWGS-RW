1884e5ca5394ac70588bc196885474108c78c8d8
Ted Wong <ted@pacificanalytics.com.au>
2018-09-18 16:14:45 +1000

    ...

--- AN1/AN1_EXTRACT.py
+++ AN1/AN1_EXTRACT.py
@@ -9,6 +9,6 @@ import sys
 keys = [ "ENSG00000204209", "ENSG00000085224", "ENSG00000164362", "ENSG00000141510" ]
 
 with open(sys.argv[1], "r") as r:
-    while line in r:
+    for line in r:
         if any(x in line for x in keys):
             print(line)
