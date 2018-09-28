6a60bec1ddcf61c4942d1965a8db1159a22344ec
Ted Wong <WongMingYin@gmail.com>
2018-09-28 15:10:03 +1000

    Update STRUCTURAL_ANNOTATION.sql

--- STRUCTURAL/STRUCTURAL_ANNOTATION.sql
+++ STRUCTURAL/STRUCTURAL_ANNOTATION.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Annotation (
 	File   		  VARCHAR(50),
 	Sample   	  VARCHAR(50),
-        Type              VARCHAR(50),
+	Type              VARCHAR(50),
 	Resolution   	  VARCHAR(50),
 	SVType    	  VARCHAR(50),
 	Chr1    	  VARCHAR(50),
